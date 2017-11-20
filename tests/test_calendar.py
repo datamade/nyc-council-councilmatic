8b096b9c5313ca4b28d8bea87c2b71b76050738b
reginafcompton <reginafcompton@gmail.com>
2017-11-20 15:04:21 -0600

    Write light test for ics file content

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -3,10 +3,19 @@ import pytest
 from django.core.urlresolvers import reverse
 
 from councilmatic_core.models import Event
-    
+from nyc.utils import create_ics_output
+  
+@pytest.mark.django_db
+def test_ics_output():
+  event = Event.objects.all().first()
+  ics_content = create_ics_output(event).decode("utf-8")
+
+  # The content should include the event name.
+  assert (event.name in ics_content) == True 
+
 @pytest.mark.django_db
 def test_ical_export(django_db_setup, client):
-    event = Event.objects.get(ocd_id='ocd-event/02481351-47b5-46c5-9401-b7ad1fc49a0b')
+    event = Event.objects.all().first()
     url = reverse('nyc:ical_export', kwargs={'slug': event.slug})
     response = client.get(url) 
 
@@ -14,3 +23,12 @@ def test_ical_export(django_db_setup, client):
     assert response.status_code == 200
     assert response['Content-Type'] == 'text/calendar'
     assert response['Content-Disposition'] == 'attachment; filename={}.ics'.format(event.slug)
+
+@pytest.mark.django_db
+def test_google_calendar_export(django_db_setup, client):
+    event = Event.objects.all().first()
+    url = reverse('nyc:google_calendar_export', kwargs={'slug': event.slug})
+    response = client.get(url) 
+
+    # The response should redirect.
+    assert response.status_code == 302
