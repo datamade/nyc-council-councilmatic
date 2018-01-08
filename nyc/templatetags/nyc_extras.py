ab86523d7cb3f9cfef115908357cb92d5fb7651b
reginafcompton <reginafcompton@gmail.com>
2018-01-08 16:42:25 -0600

    Add filter for plain text

--- nyc/templatetags/nyc_extras.py
+++ nyc/templatetags/nyc_extras.py
@@ -1,6 +1,18 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.html import strip_tags
+import re
 
 register = template.Library()
 
-# Add custom filters here. Use these decorators: @register.filter, @stringfilter.
+@register.filter
+@stringfilter
+def clean_plain_text(text):
+    paragraphs = text.replace('�', '§').split('\n')
+
+    text = ''.join('<p class="text-preview">{}</span></p>'.format(p.strip()) for p in paragraphs if is_text(p.strip()))
+
+    return text
+
+def is_text(text):
+  return (text) and ('..' not in text) and (text != '2')
