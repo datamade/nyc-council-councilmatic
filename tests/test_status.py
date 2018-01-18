f9519f6bb5931475546b64cd0357ba8f92cf56ae
Jean Cochrane <jean@jeancochrane.com>
2018-01-18 11:00:23 -0600

    Continue messing with tests

--- tests/test_status.py
+++ tests/test_status.py
@@ -1,54 +1,68 @@
-import unittest
 import datetime
 import pytz
 
 from uuid import uuid4
+import pytest
 
-from councilmatic_core.models import Bill
+from councilmatic_core.models import Bill, Action
 
+@pytest.fixture(scope='module')
+def bills():
+    '''
+    Make some bills for testing.
+    '''
+    active_bill = Bill(
+        ocd_id = 'ocd-bill/' + str(uuid4()),
+        ocd_created_at = datetime.datetime.now(pytz.utc),
+        ocd_updated_at = datetime.datetime.now(pytz.utc),
+        description = 'Active bill for testing',
+        identifier = 'T 2018-01',
+        bill_type = 'Test bill',
+        classification = 'bill',
+        source_url = '127.0.0.1:8000',
+        slug = 'active-bill'
+    )
 
-class TestBillStatus(unittest.TestCase):
+    inactive_bill = Bill(
+        ocd_id = 'ocd-bill/' + str(uuid4()),
+        ocd_created_at = datetime.datetime.now(pytz.utc),
+        ocd_updated_at = datetime.datetime.now(pytz.utc),
+        description = 'Inactive bill for testing',
+        identifier = 'T 2018-02',
+        bill_type = 'Test bill',
+        classification = 'bill',
+        source_url = '127.0.0.1:8000',
+        slug = 'inactive-bill'
+    )
 
-    @classmethod
-    def setUpClass(cls):
-        '''
-        Make some bills for testing.
-        '''
-        cls.active_bill = Bill.objects.create(
-            ocd_id = 'ocd-bill/' + str(uuid4()),
-            ocd_created_at = datetime.datetime.now(pytz.utc),
-            ocd_updated_at = datetime.datetime.now(pytz.utc),
-            description = 'Active bill for testing',
-            identifier = 'T 2018-01',
-            bill_type = 'Test bill',
-            classification = 'bill',
-            source_url = '127.0.0.1:8000',
-            slug = 'active-bill'
-        )
+    filed_bill = Bill(
+        ocd_id = 'ocd-bill/' + str(uuid4()),
+        ocd_created_at = datetime.datetime.now(pytz.utc),
+        ocd_updated_at = datetime.datetime.now(pytz.utc),
+        description = 'Filed bill for testing',
+        identifier = 'T 2018-03',
+        bill_type = 'Test bill',
+        classification = 'bill',
+        source_url = '127.0.0.1:8000',
+        slug = 'filed-bill'
+    )
 
-        cls.inactive_bill = Bill.objects.create(
-            ocd_id = 'ocd-bill/' + str(uuid4()),
-            ocd_created_at = datetime.datetime.now(pytz.utc),
-            ocd_updated_at = datetime.datetime.now(pytz.utc),
-            description = 'Inactive bill for testing',
-            identifier = 'T 2018-02',
-            bill_type = 'Test bill',
-            classification = 'bill',
-            source_url = '127.0.0.1:8000',
-            slug = 'inactive-bill'
-        )
+    active_action = Action(
+        date = datetime.datetime.now(pytz.utc),
+        classification = None,
+        _organization = None,
+        _bill = active_bill,
+        order = 1,
+    )
 
-        cls.filed_bill = Bill.objects.create(
-            ocd_id = 'ocd-bill/' + str(uuid4()),
-            ocd_created_at = datetime.datetime.now(pytz.utc),
-            ocd_updated_at = datetime.datetime.now(pytz.utc),
-            description = 'Filed bill for testing',
-            identifier = 'T 2018-03',
-            bill_type = 'Test bill',
-            classification = 'bill',
-            source_url = '127.0.0.1:8000',
-            slug = 'filed-bill'
-        )
+    yield {
+        'active_bill': active_bill,
+        'inactive_bill': inactive_bill,
+        'filed_bill': filed_bill,
+        'active_action': active_action
+    }
 
-    def test_inactive(self):
-        pass
+@pytest.mark.django_db
+def test_inactive(bills):
+    import pdb
+    pdb.set_trace()
