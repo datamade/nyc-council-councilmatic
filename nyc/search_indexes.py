eb383195f95d10a7d439e498054b04b9057ea918
Jean Cochrane <jean@jeancochrane.com>
2018-01-30 15:17:26 -0600

    Play with boosting by date

--- nyc/search_indexes.py
+++ nyc/search_indexes.py
@@ -1,4 +1,3 @@
-
 from haystack import indexes
 
 from django.conf import settings
@@ -9,6 +8,8 @@ import pytz
 from nyc.models import NYCBill
 from councilmatic_core.haystack_indexes import BillIndex
 from councilmatic_core.models import Action
+
+app_timezone = pytz.timezone(settings.TIME_ZONE)
 
 
 class NYCBillIndex(BillIndex, indexes.Indexable):
@@ -43,3 +44,18 @@ class NYCBillIndex(BillIndex, indexes.Indexable):
 
     def prepare_text(self, obj):
         return self.text.prepare(obj)
+
+    def prepare(self, obj):
+        data = super().prepare(obj)
+
+        boost = 1
+        if obj.last_action_date:
+            now = app_timezone.localize(datetime.now())
+
+            # obj.last_action_date can be in the future
+            weeks_passed = (now - obj.last_action_date).days / 7 + 1
+            boost = 1 + 5.0 / max(weeks_passed, 1)
+
+        data['boost'] = boost
+
+        return data
